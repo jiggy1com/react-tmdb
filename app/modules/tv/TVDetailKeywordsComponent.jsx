6e7dfc56fe7e5ebd73cba017e93c180cd5ac402e
Joe Velez <joe.velez@cbsinteractive.com>
2018-02-13 13:31:50 -0800

    fixing link to keyword, build out template

--- app/modules/tv/TVDetailKeywordsComponent.jsx
+++ app/modules/tv/TVDetailKeywordsComponent.jsx
@@ -1,12 +1,37 @@
 let React = require('react');
 
+let hyphenate = require('Hyphenate');
+
 let TVDetailKeywordsComponent = React.createClass({
 	
-	
 	render: function(){
+		
+		let { keywords } = this.props;
+		
 		return (
-			<div>
-				TVDetailKeywordsComponent
+			<div id={"tv-keywords"}>
+				
+				<div className={"row mb-3"}>
+					<div className={"col-12"}>
+						<h2 className={"card-header"}>Keywords</h2>
+					</div>
+				</div>
+				
+				<div className={"row mb-3"}>
+					<div className={"col-12"}>
+						{keywords.map(function(obj){
+							
+							let link = '#/keywords/' + hyphenate.hyphenateAndLowercase(obj.name) + '/' + obj.id;
+							return (
+								<a key={obj.id} href={link}  className={"badge badge-primary mr-1"}>
+									{obj.name}
+								</a>
+							)
+							
+						})}
+					</div>
+				</div>
+				
 			</div>
 		)
 	}
