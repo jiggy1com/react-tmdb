05542709b71445d91028a42741cd2bd4040019a5
Joe Velez <joe.velez@cbsinteractive.com>
2018-02-13 13:32:25 -0800

    removing class

--- app/modules/tv/TVDetailOriginCountryComponent.jsx
+++ app/modules/tv/TVDetailOriginCountryComponent.jsx
@@ -8,7 +8,7 @@ let TVDetailOriginCountryComponent = React.createClass({
 		let { originCountry } = this.props;
 		
 		return (
-			<div id={"tv-external-ids"} className={"mb-5"}>
+			<div id={"tv-external-ids"}>
 				
 				<div className={"row mb-3"}>
 					<div className={"col-12"}>
