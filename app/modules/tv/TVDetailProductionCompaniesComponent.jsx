57acb1053ee8da36b22b08bbdd4de7f88579790f
Joe Velez <joe.velez@cbsinteractive.com>
2018-02-13 13:32:47 -0800

    build out template

--- app/modules/tv/TVDetailProductionCompaniesComponent.jsx
+++ app/modules/tv/TVDetailProductionCompaniesComponent.jsx
@@ -4,9 +4,30 @@ let TVDetailProductionCompaniesComponent = React.createClass({
 	
 	
 	render: function(){
+		
+		let { productionCompanies } = this.props;
+		
 		return (
-			<div>
-				TVDetailProductionCompaniesComponent
+			<div id={"tv-credits"}>
+				
+				<div className={"row mb-3"}>
+					<div className={"col-12"}>
+						<h2 className={"card-header"}>Production Companies</h2>
+					</div>
+				</div>
+				
+				<div className={"row mb-3"}>
+					<div className={"col-12"}>
+						{productionCompanies.map(function(obj){
+							return (
+								<div key={obj.id}>
+									{obj.name}
+								</div>
+							)
+						})}
+					</div>
+				</div>
+				
 			</div>
 		)
 	}
