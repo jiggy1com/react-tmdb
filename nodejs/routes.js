ad007ff4c188b9e063e6d13ef84194930a1f2975
Joe Velez <joe.velez@cbsinteractive.com>
2018-02-24 21:24:59 -0800

    remove unnecessary routes or commented out routes, adding genre and keyword routes

--- nodejs/routes.js
+++ nodejs/routes.js
@@ -6,6 +6,8 @@ let MovieController = require('./MovieController');
 let TVController = require('./TVController');
 let PersonController = require('./PersonController');
 let SearchController = require('./SearchController');
+let GenreController = require('./GenreController');
+let KeywordController = require('./KeywordController');
 
 // routes
 module.exports = function(app) {
@@ -19,8 +21,8 @@ module.exports = function(app) {
 	app.get('//api/v1/discover/movie', MovieController.getDiscoverMovie);
 	
 	// TODO: make a page for these then add the link to the nav
-	app.get('/api/v1/movie/latest', function(req, res, next){console.log('ok'); next();}, MovieController.getLatest);
-	app.get('/api/v1/movie/latest/:page', function(req, res, next){console.log('ok'); next();}, MovieController.getLatest);
+	// app.get('/api/v1/movie/latest', function(req, res, next){console.log('ok'); next();}, MovieController.getLatest);
+	// app.get('/api/v1/movie/latest/:page', function(req, res, next){console.log('ok'); next();}, MovieController.getLatest);
 	
 	app.get('/api/v1/movie/reviews/:movieId', MovieController.getReviews);
 	app.get('/api/v1/movie/videos/:movieId', MovieController.getVideos);
@@ -29,60 +31,45 @@ module.exports = function(app) {
 	app.get('/api/v1/movie/keywords/:movieId', MovieController.getKeywords);
 	app.get('/api/v1/movie/release-dates/:movieId', MovieController.getReleaseDates);
 	app.get('/api/v1/movie/recommendations/:movieId', MovieController.getRecommendations);
-	app.get('/api/v1/movie/similar/:movieId', MovieController.getRecommendations);
-	
-	// app.get('/api/v1/movie/details/:movieId', MovieController.getMovie);
+	app.get('/api/v1/movie/similar/:movieId', MovieController.getSimilar);
 	app.get('/api/v1/movie/:movieId', MovieController.getMovie);
-	
 	app.get('/api/v1/movie/:category/:page', MovieController.getMoviesByCategory);
-	// app.get('/api/v1/movie/now_playing/:page', MovieController.getMoviesByCategory);
-	// app.get('/api/v1/movie/popular/:page', MovieController.getMoviesByCategory);
-	// app.get('/api/v1/movie/top_rated/:page', MovieController.getMoviesByCategory);
-	// app.get('/api/v1/movie/upcoming/:page', MovieController.getMoviesByCategory);
-	
-	
-	
 	
 	//
 	// TV
 	//
 	
 	// tv details
-	// 1418
 	app.get('/api/v1/tv/detail/:tvId', TVController.getTVDetail);
-	
-	// app.get('/api/v1/tv//images/:tvId', TVController.getImages);
-	// app.get('/api/v1/tv//content_ratings/:tvId', TVController.getContentRatings);
-	// app.get('/api/v1/tv//credits/:tvId', TVController.getCredits);
-	// app.get('/api/v1/tv//external_ids/:tvId', TVController.getExternalIds);
-	// app.get('/api/v1/tv//keywords/:tvId', TVController.getKeywords);
-	// app.get('/api/v1/tv//recommendations/:tvId', TVController.getRecommendations);
-	// // app.get('/api/v1/tv//screened_theatrically/:tvId', TVController.getTest);
-	// app.get('/api/v1/tv//similar/:tvId', TVController.getSimilar);
-	// app.get('/api/v1/tv//videos/:tvId', TVController.getVideos);
-	// app.get('/api/v1/tv/detail/:endpoint/:tvId', TVController.getTest);
 	
 	// tv list
 	app.get('/api/v1/tv/:category/:page', TVController.getTVByCategory);
 	
-	
+	// tv season details
+	app.get('/api/v1/tv/season/:tvId/:seasonNumber', TVController.getTVSeason);
 	
 	//
 	// People
 	//
+	
 	app.get('/api/v1/person/:personId', PersonController.getPerson);
 	
 	//
 	// Search
 	//
+	
 	app.post('/api/v1/search/multi', SearchController.getMulti);
 	
 	//
-	//
+	// Genre
 	//
 	
-	// app.get('/', MovieController.test);
-	// app.get('/', MovieController.test);
-	// app.get('/', MovieController.test);
+	app.get('/api/v1/genre/:genreType/:id/:page', GenreController.getByGenre);
+	
+	//
+	// Keywords
+	//
+	
+	app.get('/api/v1/keyword/:keywordType/:id/:page', KeywordController.getByKeyword);
 	
 };
