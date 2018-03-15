49e54b47d9db3071355ee4aa7d6f1fe0e0bccfa5
Joe Velez <joe.velez@cbsinteractive.com>
2018-03-15 13:05:50 -0700

    adding display movie list item

---
+++ app/components/DspMovieListItem.jsx
@@ -0,0 +1,16 @@
+let React = require('react');
+let { Link } = require('react-router');
+
+let DspMovieListItem = React.createClass({
+	
+	render: function(){
+		return (
+			<div>
+				DspMovieListItem
+			</div>
+		)
+	}
+	
+});
+
+module.exports = DspMovieListItem;
