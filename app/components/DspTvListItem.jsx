e5b03cafbd02aa4e490d9999716effecdaa83892
Joe Velez <joe.velez@cbsinteractive.com>
2018-03-15 13:06:01 -0700

    adding display tv list item

---
+++ app/components/DspTvListItem.jsx
@@ -0,0 +1,16 @@
+let React = require('react');
+let { Link } = require('react-router');
+
+let DspTvListItem = React.createClass({
+	
+	render: function(){
+		return (
+			<div>
+				DspTvListItem
+			</div>
+		)
+	}
+	
+});
+
+module.exports = DspTvListItem;
