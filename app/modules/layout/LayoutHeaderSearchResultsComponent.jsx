cf5f34dec06b35c467fd88152a331ae02a93e594
Joe Velez <joe.velez@cbsinteractive.com>
2018-03-15 13:03:59 -0700

    adding more specific search results

--- app/modules/layout/LayoutHeaderSearchResultsComponent.jsx
+++ app/modules/layout/LayoutHeaderSearchResultsComponent.jsx
@@ -4,76 +4,90 @@ let hyphenate = require('Hyphenate');
 
 let LayoutHeaderSearchResultsComponent = React.createClass({
 	
-	filterResults: function(nextProps){
-		
-		let movieList = [];
-		let tvList = [];
-		let personList = [];
-		
-		nextProps.results.forEach(function(obj){
-			if(obj.media_type === 'movie'){
-				movieList.push(obj);
-			}
-		});
-		
-		nextProps.results.forEach(function(obj){
-			if(obj.media_type === 'tv'){
-				tvList.push(obj);
-			}
-		});
-		
-		nextProps.results.forEach(function(obj){
-			if(obj.media_type === 'person'){
-				personList.push(obj);
-			}
-		});
-		
-		let update = {
-			movieList: movieList,
-			tvList: tvList,
-			personList: personList
-		};
-		
-		this.setState(update);
-		
-	},
-	
+	// @Deprecated
+	// filterResults: function(nextProps){
+	//
+	// 	let movieList = [];
+	// 	let tvList = [];
+	// 	let personList = [];
+	//
+	// 	nextProps.results.forEach(function(obj){
+	// 		if(obj.media_type === 'movie'){
+	// 			movieList.push(obj);
+	// 		}
+	// 	});
+	//
+	// 	nextProps.results.forEach(function(obj){
+	// 		if(obj.media_type === 'tv'){
+	// 			tvList.push(obj);
+	// 		}
+	// 	});
+	//
+	// 	nextProps.results.forEach(function(obj){
+	// 		if(obj.media_type === 'person'){
+	// 			personList.push(obj);
+	// 		}
+	// 	});
+	//
+	// 	let update = {
+	// 		movieList: movieList,
+	// 		tvList: tvList,
+	// 		personList: personList
+	// 	};
+	//
+	// 	this.setState(update);
+	//
+	// },
+
 	getInitialState: function(){
 		return {
 			movieList : [],
 			tvList: [],
-			personList: []
+			personList: [],
+			search: ''
 		}
 	},
 	
 	getDefaultProps: function(){
 		return {
-			results : [],
+			
+			movieList: [],
+			tvList: [],
+			personList: [],
+			search: '',
+			
 			onClose : function(){
-				console.log('close was not passed in');
+				console.log('close was not passed in. This prop should be passed in to override THIS default prop.');
 			}
 		}
 	},
 	
 	componentWillReceiveProps: function(nextProps){
-		this.filterResults(nextProps);
+		// this.filterResults(nextProps);
+		this.setState({
+			movieList: nextProps.movieList,
+			tvList: nextProps.tvList,
+			personList: nextProps.personList,
+			search: nextProps.search
+		});
 	},
 	
 	renderMovieList: function(){
-		let { movieList } = this.state;
+		let { movieList, search } = this.state;
 		let { onClose } = this.props;
 		
 		if(movieList.length === 0){
 			return (
 				<div>
+					Sorry, no results for {search}.
 				</div>
 			)
 		}else{
 			return movieList.map(function(obj, idx){
 				let href = '/movie/detail/' + hyphenate.hyphenateAndLowercase(obj.title) + '/' + obj.id;
 				return (
-					<li key={idx}>
-						<Link to={href} onClick={onClose}>
+					<li key={idx} className={"text-light"}>
+						<Link to={href} onClick={onClose} className={"text-light"}>
 							{obj.title}
 						</Link>
 					</li>
@@ -83,20 +97,21 @@ let LayoutHeaderSearchResultsComponent = React.createClass({
 	},
 	
 	renderTvList: function(){
-		let { tvList } = this.state;
+		let { tvList, search } = this.state;
 		let { onClose } = this.props;
 		
 		if(tvList.length === 0){
 			return (
 				<div>
+					Sorry, no results for {search}.
 				</div>
 			)
 		}else{
 			return tvList.map(function(obj, idx){
 				let href = '/tv/detail/' + hyphenate.hyphenateAndLowercase(obj.name) + '/' + obj.id;
 				return (
-					<li key={idx}>
-						<Link to={href} onClick={onClose}>
+					<li key={idx} className={"text-light"}>
+						<Link to={href} onClick={onClose} className={"text-light"}>
 							{obj.name}
 						</Link>
 					</li>
@@ -106,20 +121,21 @@ let LayoutHeaderSearchResultsComponent = React.createClass({
 	},
 	
 	renderPersonList: function(){
-		let { personList } = this.state;
+		let { personList, search } = this.state;
 		let { onClose } = this.props;
 		
 		if(personList.length === 0){
 			return (
 				<div>
+					Sorry, no results for {search}.
 				</div>
 			)
 		}else{
 			return personList.map(function(obj, idx){
 				let href = '/person/' + hyphenate.hyphenateAndLowercase(obj.name) + '/' + obj.id;
 				return (
-					<li key={idx}>
-						<Link to={href} onClick={onClose}>
+					<li key={idx} className={"text-light"}>
+						<Link to={href} onClick={onClose} className={"text-light"}>
 							{obj.name}
 						</Link>
 					</li>
@@ -133,9 +149,9 @@ let LayoutHeaderSearchResultsComponent = React.createClass({
 		let movieHtml = this.renderMovieList();
 		let tvHtml = this.renderTvList();
 		let personHtml = this.renderPersonList();
-		let { results } = this.props;
+		let { movieList, tvList, personList } = this.props;
 		
-		if(results.length === 0){
+		if(movieList.length === 0 && tvList.length === 0 && personList.length === 0){
 			return (
 				<div>
 				</div>
@@ -146,19 +162,19 @@ let LayoutHeaderSearchResultsComponent = React.createClass({
 					<div className={"container"}>
 						<div className={"row"}>
 							<div className={"col"}>
-								<h2>Movies</h2>
+								<h2 className={"text-light"}>Movies</h2>
 								<ul>
 									{movieHtml}
 								</ul>
 							</div>
 							<div className={"col"}>
-								<h2>TV Shows</h2>
+								<h2 className={"text-light"}>TV Shows</h2>
 								<ul>
 									{tvHtml}
 								</ul>
 							</div>
 							<div className={"col"}>
-								<h2>Actors</h2>
+								<h2 className={"text-light"}>Actors</h2>
 								<ul>
 									{personHtml}
 								</ul>
