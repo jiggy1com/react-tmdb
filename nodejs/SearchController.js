d6ab14bb3dd6c5d3e2ad29361f7b51df0864a6fc
Joe Velez <joe.velez@cbsinteractive.com>
2018-03-15 13:05:21 -0700

    adding methods for searching

--- nodejs/SearchController.js
+++ nodejs/SearchController.js
@@ -20,3 +20,60 @@ exports.getMulti = function(req, res){
 	api.doRequest(req, res, options);
 	
 };
+
+exports.getMovie = function(req, res){
+	
+	console.log('body', req.body);
+	
+	let obj = {
+		path : 'search/movie',
+		method : 'GET',
+		qs : {
+			query : req.body.search,
+			page : 1
+		}
+	};
+	
+	let options = api.setOptions(obj);
+	
+	api.doRequest(req, res, options);
+	
+};
+
+exports.getTV = function(req, res){
+	
+	console.log('body', req.body);
+	
+	let obj = {
+		path : 'search/tv',
+		method : 'GET',
+		qs : {
+			query : req.body.search,
+			page : 1
+		}
+	};
+	
+	let options = api.setOptions(obj);
+	
+	api.doRequest(req, res, options);
+	
+};
+
+exports.getPerson = function(req, res){
+	
+	console.log('body', req.body);
+	
+	let obj = {
+		path : 'search/person',
+		method : 'GET',
+		qs : {
+			query : req.body.search,
+			page : 1
+		}
+	};
+	
+	let options = api.setOptions(obj);
+	
+	api.doRequest(req, res, options);
+	
+};
